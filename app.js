956a629a6af00e4c6bbb287a51c9e8f21b864fa9
Rajat Pratap Singh <37918319+rajatpratapsingh@users.noreply.github.com>
2020-03-09 08:42:33 +0530

    add sql db

--- app.js
+++ app.js
@@ -3,16 +3,28 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const errorController = require('./controllers/error');
+const errorController = require("./controllers/error");
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', 'views')
+const db = require("./util/database");
+
+/* db.execute("SELECT * from products")
+  .then(result => {
+    console.log("result", result[0]);
+  })
+  .catch(err => {
+    console.log("error", err);
+  }); */
+
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 /* https://stackoverflow.com/questions/38306569/what-does-body-parser-do-with-express */
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
-app.use(express.static(path.join(__dirname, "public"))); /* target static .css styling */
+app.use(
+  express.static(path.join(__dirname, "public"))
+); /* target static .css styling */
 app.use(errorController.get404);
 
 app.listen(3000);
